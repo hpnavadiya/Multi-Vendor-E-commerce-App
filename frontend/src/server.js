31e5a22fcfd679412eac3072ed567589dfdc1feb
hpnavadiya <navadiyaharsh8@gmail.com>
2024-02-13 02:12:05 -0500

    Upload backend live url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://multi-vendor-e-commerce-app.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://multi-vendor-e-commerce-app.vercel.app/";
 
 
 
